434da300366093a634a47f88ab7633b359d14630
Manvitha <manvi15.challagundla@gmail.com>
2025-03-26 16:48:17 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp9
+{
+    public class Person
+    {
+        public string Name;
+        public string Email;
+        public string ID;
+
+        public Person(string name, string email, string id)
+        {
+            Name = name;
+            Email = email;
+            ID = id;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Email: {Email}, ID: {ID}");
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            
+        }
+    }
+}
